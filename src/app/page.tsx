efe5e38f67df78fcca7ea09516c0f98dbc6db7e2
mqasim675 <qm31480@gmail.com>
2023-12-28 15:36:31 +0500

    third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,70 @@ export default function Home() {
   return (
     <>
       <div>
-        <Typography variant="displayXl" color="Gray1" fw="bold" as="h1" center>
+        <Typography
+          variant="displayXl"
+          color="Gray1"
+          fw="exbold"
+          as="h1"
+          center
+        >
           Heading 1
+        </Typography>
+        <Typography variant="displayLg" color="Gray1" fw="bold" as="h2" center>
+          Heading 2
+        </Typography>
+        <Typography variant="displayMd" color="Gray1" fw="bold" as="h3" center>
+          Heading 3
+        </Typography>
+        <Typography variant="displaySm" color="Gray1" fw="bold" as="h4" center>
+          Heading 4
+        </Typography>
+        <Typography variant="displayXs" color="Gray1" fw="bold" as="h5" center>
+          Heading 5
+        </Typography>
+        <Typography variant="displayXxs" color="Gray1" fw="bold" as="h6" center>
+          Heading 6
+        </Typography>
+        {/* textXl */}
+        <Typography variant="textXl" color="Gray1" fw="bold" as="p" center>
+          Large Text Bold
+        </Typography>
+        <Typography variant="textXl" color="Gray1" fw="regular" as="p" center>
+          Large Text Regular
+        </Typography>
+        {/* textLg */}
+        <Typography variant="textLg" color="Gray1" fw="bold" as="p" center>
+          Medium Text Bold
+        </Typography>
+        <Typography variant="textLg" color="Gray1" fw="regular" as="p" center>
+          Medium Text Regular
+        </Typography>
+        {/* textMd */}
+        <Typography variant="textMd" color="Gray1" fw="bold" as="p" center>
+          Normal Text Bold
+        </Typography>
+        <Typography
+          variant="displayXxs"
+          color="Gray1"
+          fw="regular"
+          as="p"
+          center
+        >
+          Normal Text Regular
+        </Typography>
+        {/* textSm */}
+        <Typography variant="textSm" color="Gray1" fw="bold" as="p" center>
+          Small Text Bold
+        </Typography>
+        <Typography variant="textSm" color="Gray1" fw="regular" as="p" center>
+          Small Text Regular
+        </Typography>
+        {/* textXs */}
+        <Typography variant="textXs" color="Gray1" fw="bold" as="p" center>
+          Small Text Bold
+        </Typography>
+        <Typography variant="textXs" color="Gray1" fw="regular" as="p" center>
+          Small Text Regular
         </Typography>
       </div>
     </>
--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -38,7 +38,19 @@ export const Typography: FC<TypographyProps> = ({
           ["text-center"]: center,
           ["text-[3rem] leading-[130%]"]: variant === "displayXl",
           ["text-gray-1"]: color === "Gray1",
+          ["font-extrabold"]: fw === "exbold",
           ["font-bold"]: fw === "bold",
+          ["font-normal"]: fw === "regular",
+          ["text-4xl leading-[150%]"]: variant === "displayLg",
+          ["text-3xl leading-[150%]"]: variant === "displayMd",
+          ["text-2xl leading-[150%]"]:
+            variant === "displaySm" || variant === "textXl",
+          ["text-xl leading-[150%]"]: variant === "displayXs",
+          ["text-lg leading-[150%]"]:
+            variant === "displayXxs" || variant === "textLg",
+          ["text-base leading-[150%]"]: variant === "textMd",
+          ["text-sm leading-[150%]"]: variant === "textSm",
+          ["text-xs leading-[150%]"]: variant === "textXs",
         })}
       >
         {children}
